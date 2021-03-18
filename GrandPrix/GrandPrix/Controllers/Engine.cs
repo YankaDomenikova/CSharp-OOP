c5d6fe0712a33f66ecdba5694e85c2c0adde91cf
Yanka Domenikova <yankadomenikova@gmail.com>
2021-03-18 07:23:46 +0200

    add grand prix project

---
+++ GrandPrix/GrandPrix/Controllers/Engine.cs
@@ -0,0 +1,20 @@
+﻿namespace GrandPrix.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Engine
+    {
+        public static void Run()
+        {
+            var raceTower = new RaceTower();
+            
+            int lapsNumber = int.Parse(Console.ReadLine());
+            int lenghtTrack = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ GrandPrix/GrandPrix/Controllers/RaceTower.cs
@@ -0,0 +1,41 @@
+﻿namespace GrandPrix.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RaceTower
+    {
+        public void SetTrackInfo(int lapsNumber, int trackLength)
+        {
+            //TODO: Add some logic here …
+        }
+        public void RegisterDriver(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public void DriverBoxes(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string CompleteLaps(List<string> commandArgs)
+        {
+            return null;
+        }
+
+        public string GetLeaderboard()
+        {
+            return null;
+        }
+
+        public void ChangeWeather(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+        }
+
+    }
+}
